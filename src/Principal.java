32c2d4c7ed630709602f42d1d4b131f25cb35d6b
Jonathan Ferreira <jonathan2016bento22@gmail.com>
2025-08-29 16:41:21 -0300

    feat: passo3-7/12

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,17 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.math.RoundingMode;
 
 public class Principal {
+
+    
     public static void main(String[] args) {
+
+        // 3.1 - Preencher tabela de funcionários
         List<Funcionario> funcionarios = new ArrayList<>();
         funcionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
         funcionarios.add(new Funcionario("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
@@ -16,5 +23,46 @@ public class Principal {
         funcionarios.add(new Funcionario("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"));
         funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"));
         funcionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
+
+        // 3.2 - Remover funcionário João
+        funcionarios.removeIf(f -> f.getNome().equalsIgnoreCase("João"));
+
+        // 3.3 - Imprimir informações de funcionários
+        System.out.println("\n- Funcionários -");
+        imprimirFuncionarios(funcionarios);
+
+        // 3.4 - Aumento de 10% nos salários
+        funcionarios.forEach(f -> f.setSalario(f.getSalario().multiply(new BigDecimal("1.10")).setScale(2, RoundingMode.HALF_UP)));
+
+        System.out.println("\n- Funcionários: Após aumento de 10% -");
+        imprimirFuncionarios(funcionarios);
+
+
+        // 3.5 - Agrupar em um MAP
+        // 3.6 – Imprimir os funcionários agrupados por função
+        // Não conheço o método MAP, vou prosseguir alguns estudos a respeito
+        
+
+        // 3.7 - Imprimir funcionários que fazem aniversário no mês 10 e 12
+        List<Funcionario> aniversariooutedec = funcionarios.stream() 
+            .filter (f -> f.getDataNascimento().getMonthValue() == 10 || f.getDataNascimento().getMonthValue() == 12)
+            .collect(Collectors.toList());
+
+        System.out.println("\n- Aniversariantes de Outubro e Dezembro -");
+        imprimirFuncionarios(aniversariooutedec);
+
     }
+
+    public static void imprimirFuncionarios(List<Funcionario> funcionarios) {
+    for (Funcionario f : funcionarios) {
+        System.out.printf(
+            "Nome: %s | Nascimento: %s | Função: %s | Salário: %s%n",
+            f.getNome(),
+            f.getDataNascimento().format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+            f.getFuncao(),
+            String.format("%,.2f", f.getSalario())
+        );
+    }
+}
+
 }
